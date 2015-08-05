682f5a4e719030bfff528fe3fdaa44d9d2ca2926
codin <codin@qq.com>
2015-08-05 09:48:28 +0800

    update main

--- main.go
+++ main.go
@@ -8,31 +8,27 @@ import (
 	"github.com/codinl/gotpl/gotpl"
 )
 
-//func Usage() {
-//	fmt.Fprintf(os.Stderr, "usage: gorazor [-debug] [-watch] <input dir or file> <output dir or file>\n")
-//	flag.PrintDefaults()
-//	os.Exit(1)
-//}
+func Usage() {
+	fmt.Fprintf(os.Stderr, "usage: gotpl [-debug] [-watch] <input dir> <output dir>\n")
+	flag.PrintDefaults()
+	os.Exit(1)
+}
 
 func main() {
-//	flag.Usage = Usage
-//	isDebug := flag.Bool("debug", false, "use debug mode")
-//	isWatch := flag.Bool("watch", false, "use watch mode")
-//	nameNotChange := flag.Bool("nameNotChange", false, "do not change name of the template")
+	flag.Usage = Usage
+	isDebug := flag.Bool("debug", false, "use debug mode")
+	isWatch := flag.Bool("watch", false, "use watch mode")
 
-//	flag.Parse()
+	flag.Parse()
 
 	options := gotpl.Option{}
 
-//	if *isDebug {
-//		options["Debug"] = *isDebug
-//	}
-//	if *isWatch {
-//		options["Watch"] = *isWatch
-//	}
-//	if *nameNotChange {
-//		options["NameNotChange"] = *nameNotChange
-//	}
+	if *isDebug {
+		options["Debug"] = *isDebug
+	}
+	if *isWatch {
+		options["Watch"] = *isWatch
+	}
 
 	options["Debug"] = false
 	options["Watch"] = false
@@ -41,8 +37,7 @@ func main() {
 		flag.Usage()
 	}
 
-//	input, output := flag.Arg(0), flag.Arg(1)
-	input, output := "tpl/", "gen/"
+	input, output := flag.Arg(0), flag.Arg(1)
 	stat, err := os.Stat(input)
 	if err != nil {
 		fmt.Println(err)
@@ -50,17 +45,9 @@ func main() {
 	}
 
 	if stat.IsDir() {
-		//		fmt.Printf("Gorazor processing dir: %s -> %s\n", input, output)
-		//		err := gotpl.GenFolder(input, output, options)
-		//		if err != nil {
-		//			fmt.Println(err)
-		//		}
 		err := gotpl.Generate(input, output, options)
 		if err != nil {
 			fmt.Println(err)
 		}
-	} else if stat.Mode().IsRegular() {
-		//		fmt.Printf("Gorazor processing file: %s -> %s\n", input, output)
-		//		gotpl.GenFile(input, output, options)
 	}
 }
