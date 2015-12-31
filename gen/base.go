5ec3a6905e40b052ecd661892379bb2e48babfb9
codin <codin@qq.com>
2015-12-31 15:42:34 +0800

    update Readme and samples

--- gen/base.go
+++ gen/base.go
@@ -2,10 +2,13 @@ package tpl
 
 import (
 	"bytes"
+	"github.com/codinl/gotpl/template"
 )
 
-func Base() []byte {
+func Base(val int) []byte {
 	var _buffer bytes.Buffer
-	_buffer.WriteString("\n\n\naaa")
+	_buffer.WriteString("\n\n<html>\n<body>\n\n    <div>base aa content</div>\n\n    \n        <div>\"base bb content\"</div>\n    \n\n    \n        <div>base cc content</div>\n    \n\n\n<div>this is TestSection content. Param \"val\" is: ")
+	_buffer.WriteString(gotpl.HTMLEscape(val))
+	_buffer.WriteString(" </div>\n\n\n</body>\n\n</html>")
 	return _buffer.Bytes()
 }
--- gen/child.go
+++ gen/child.go
@@ -2,10 +2,20 @@ package tpl
 
 import (
 	"bytes"
+	"github.com/codinl/gotpl/template"
 )
 
-func Child() []byte {
+func Child(val int) []byte {
 	var _buffer bytes.Buffer
-	_buffer.WriteString("\n\n    child\n\n\naaa")
+	_buffer.WriteString("\n\n<html>\n<body>\n\n    <div>base aa content</div>\n\n    \n    <div>child bb block content.</div>\n\n\n    \n    ")
+	for i := 0; i < 10; i++ {
+
+		_buffer.WriteString("<p>")
+		_buffer.WriteString(gotpl.HTMLEscape(i))
+		_buffer.WriteString("</p>\n    ")
+	}
+	_buffer.WriteString("\n\n\n\n<div>this is TestSection content. Param \"val\" is: ")
+	_buffer.WriteString(gotpl.HTMLEscape(val))
+	_buffer.WriteString(" </div>\n\n\n</body>\n\n</html>")
 	return _buffer.Bytes()
 }
